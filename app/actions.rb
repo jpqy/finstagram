dedca9f5fbb54635cdcbcd2132f1624b350c7e6f
Jimmy Peng <jimmyp22@gmail.com>
2022-10-27 10:08:12 +0000

    Finish W4D2

--- app/actions.rb
+++ app/actions.rb
@@ -1,15 +1,26 @@
-def humanized_time_ago(minute_num)
-  if minute_num >= 60
-    "#{minute_num / 60} hours ago"
-  else
-    "#{minute_num} minutes ago"
-  end
-end
-
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
-
   erb(:index)
+end
+
+get '/signup' do     # if a user navigates to the path "/signup",
+  @user = User.new   # setup empty @user object
+  erb(:signup)       # render "app/views/signup.erb"
+end
+
+post '/signup' do
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  if @user.save
+    "User #{username} saved!"
+  else
+    erb(:signup)
+  end
 end
 
 get '/pokemons' do
@@ -30,7 +41,6 @@ end
 
 # Handle form submission
 post '/trainers' do
-
   new_trainer = Trainer.new(name: params["trainer_name"], image_url: params["image_url"])
   if new_trainer.save
     redirect "/trainers"
